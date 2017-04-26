d904f9b64b5c943ccbadc2d6371556f8ee617b54
Cato Danielsen <catodanielsen@gmail.com>
2017-04-26 08:06:04 +0200

    restructure

--- main.py
+++ main.py
@@ -6,57 +6,71 @@ from urllib.request import urlopen
 from pprint import pprint
 import http
 
-class myGithubRepos:
-	githubUserName = ''
-	fileName = ''
 
-	def __init__(self, githubUserName):
-		self.githubUserName = githubUserName
-		self.fileName =  githubUserName+'_repos.json'
-		print('ohyeah')
-
-	def collectRepos(self):
-		print('*** Collecting public for user ' + self.githubUserName)
-
-		u = urlopen('https://api.github.com/users/'+self.githubUserName+'/repos')
-		resp = json.loads(u.read().decode('utf-8'))
-		#pprint(resp)
-		return resp
-
-	def writeToFile(self, data):
-		print('*** Writing to file ' + self.fileName)
-
-		#json_str = json.dumps(data)
-		with open(self.fileName, 'w') as f:
-			json.dump(data, f,indent=4)
-		#print(data)
-
-##########################
-
+###
+# Getting started with python3
 def main():
+	username = extractArgs()
+	
+	repo_json = repos.collectReposFor(username)
+	
+	reduceData(repo_json)
+
+#	writeToFile(username+'_repos.json')
+
+### 
+# Ask github for public repos and return as list
+def collectReposFor(self, username):
+	print('*** Collecting public repos, username: ' + username)
+
+	u = urlopen('https://api.github.com/users/'+username+'/repos')
+	resp = json.loads(u.read().decode('utf-8'))
+	#pprint(resp)
+	print(type(resp))
+	return resp
+
+
+###
+# Boil the list down to active repos
+def reduceData(orginial_json):
+	reduced_info = list()
+	current_obj = {}
+
+
+	print(type(orginial_json))
+	print(len(orginial_json))
+
+	return ''
+
+### 
+# Stores data as json-file
+def writeToFile(filename, data):
+	print('*** Writing to file ' + filename)
+
+	with open(filename, 'w') as f:
+		json.dump(data, f,indent=4)
+	#print(data)
+
+
+### 
+# Reads arguments and tries to find username
+def extractArgs():
 	grabUser = False
 	for arg in sys.argv: # Akk, hairy stuff øøøø
 		if arg == '-u':
 			grabUser = True # read the next as username
 			continue
 		elif grabUser == True:
-			userName = arg
+			username = arg
 			grabUser = False
-	
+	print(http.client)
 	try:
-	  userName
+		username
 	except NameError:
-	  print('No user name has been given, please use \'-u <uName>\'')
-	  sys.exit()
+		print('No user name has been given, please use \'-u <uName>\'')
+		sys.exit()
 
-	repos = myGithubRepos(userName)
-	repo_json = repos.collectRepos()
-
-	
-	print(len(repo_json))
-
-	#repos.writeToFile(repo_json)
-
+	return username
 
 
 if __name__ == '__main__':
